cb24527ca3f6c22ee2e24c730d05cf5643e08190
Anna Dolbina <anna.dolbina@gmail.com>
2012-11-20 10:45:34 +0200

    Lesson 1. Step 1,2,3,4,5. No Maven configuration yet

--- Calculator/ConsoleClient/src/CalculatorConsoleClient.java
+++ Calculator/ConsoleClient/src/CalculatorConsoleClient.java
@@ -7,13 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Администратор
- * Date: 11.11.12
- * Time: 18:22
- * To change this template use File | Settings | File Templates.
- */
+
 public class CalculatorConsoleClient {
     public static void main(String[] args){
  try {
@@ -24,9 +18,9 @@ public class CalculatorConsoleClient {
             BigDecimal result= calculator.evaluate(expression);
             System.out.println("Результат вычислений: "+result.doubleValue());
         } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            e.printStackTrace();
         } catch (CompilationException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                e.printStackTrace();
         }
     }
 }
